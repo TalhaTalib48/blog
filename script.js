b3e4172a8918cb225c28be55eefbc6808401d4a1
TalhaTalib48 <tt1606224@gmail.com>
2024-12-30 19:50:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function one(){
+     alert("Welcome to my website");
+}
+ function color_change1(){
+ 	document.getElementById("change_color1").style.color='navy';
+ }
+ function color_BackChange1(){
+   document.getElementById("change_color1").style.color='black';
+   
+}
+
+
+function color_change2() {
+  document.getElementById("color_change2").style.color = 'navy';
+}
+
+function color_BackChange2() {
+ document.getElementById("color_change2").style.color = 'black';
+}
